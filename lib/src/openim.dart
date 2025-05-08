661d979e3e17ff3579b6f54812457868a0657afa
zhuangyuhao <zhuangyuhao@zhuangyuhaodeMacBook-Pro.local>
2025-05-08 17:44:38 +0800

    feat: update core-sdk

--- lib/src/openim.dart
+++ lib/src/openim.dart
@@ -2,7 +2,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_openim_sdk/flutter_openim_sdk.dart';
 
 class OpenIM {
-  static const version = '3.4.0+21';
+  static const version = '3.4.0+22';
 
   static const _channel = const MethodChannel('flutter_openim_sdk');
 
